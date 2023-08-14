e154709ef4f1c6e53247a4d3b28e5038b7a815cf
NEILSON Michael <tt6469@engie.com>
2023-08-14 17:12:48 +0200

    Boilerplate response

--- src/app/adapters/strava_api_adapter.py
+++ src/app/adapters/strava_api_adapter.py
@@ -3,3 +3,5 @@ import requests
 class StravaAPIAdapter:
     def get_strava_data():
         session = requests.session()
+        resp = session.get("https://google.com")
+        return resp.json()
