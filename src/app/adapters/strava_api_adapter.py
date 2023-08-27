fb3dfff1a7d476a54c15e4469e1b1e11fea1f977
NEILSON Michael <tt6469@engie.com>
2023-08-27 22:32:51 +0200

    Continue setting up strava auth token

--- src/app/adapters/strava_api_adapter.py
+++ src/app/adapters/strava_api_adapter.py
@@ -1,7 +1,39 @@
 import requests
+import yaml
+import webbrowser
+
+def read_config() -> dict:
+    with open('./src/config.yaml', 'r') as stream:
+        try: 
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            raise e
 
 class StravaAPIAdapter:
-    def get_strava_data():
-        session = requests.session()
-        resp = session.get("https://google.com")
+    def __init__(self) -> None:
+        config = read_config()
+        self.client_id: str = config["strava_credentials"]["client_id"]
+        self.session: requests.Session = requests.session()
+
+    def authorize_strava_api(self):
+        url = f"https://www.strava.com/oauth/authorize"
+        redirect_uri = "http://localhost/exchange_token"
+
+        auth_url = f"{url}?client_id={self.client_id}&redirect_uri={redirect_uri}&response_type=code&scope=read&approval_prompt=force"
+        # resp = self.session.get(url=url, params=params, verify=False) # TODO: remove 'verify' later
+        webbrowser.open(auth_url)
+        pass
+
+    def get_strava_athlete_activities(self):
+        session = self.session()
+        URL = "https://www.strava.com/api/v3/athlete/activities"
+        headers = {"Authorization": "Bearer "}
+        params = {
+            "before": 1692061200,
+            "after": 1672534800
+        }
+        resp = session.get(URL, headers=headers, params=params)
         return resp.json()
+    
+    def get_strava_activity_timeseries():
+        pass
---
+++ tests/strava_api.py
@@ -0,0 +1,4 @@
+from src.app.adapters.strava_api_adapter import StravaAPIAdapter
+
+if __name__ == "__main__":
+    StravaAPIAdapter().authorize_strava_api()
