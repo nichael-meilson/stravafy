2be90b88b82f15f6e2fbc09ad01586bd57820512
nichael-meilson <michaelshawnneilson@gmail.com>
2023-08-28 20:57:02 +0200

    Strava API connection complete

--- src/app/adapters/strava_api_adapter.py
+++ src/app/adapters/strava_api_adapter.py
@@ -2,6 +2,8 @@ import requests
 import yaml
 import webbrowser
 from src.utils.encryption import Encryption
+from src.utils.strava_auth_handler import StravaAuthHandler
+from starlette.exceptions import HTTPException
 
 def read_config() -> dict:
     with open('./src/config.yaml', 'r') as stream:
@@ -11,28 +13,52 @@ def read_config() -> dict:
             raise e
 
 class StravaAPIAdapter:
+    client_id: str
+    session: requests.Session
+    access_token: str
+
     def __init__(self) -> None:
         config = read_config()
         self.client_id: str = Encryption().decrypt_string(config["strava_credentials"]["client_id"])
         self.session: requests.Session = requests.session()
 
-    def authorize_strava_api(self):
+    def authorize_strava_api(self) -> str:
         url = f"https://www.strava.com/oauth/authorize"
-        redirect_uri = "https://localhost/exchange_token"
+        redirect_uri = "http://localhost:5000"
 
-        auth_url = f"{url}?client_id={self.client_id}&redirect_uri={redirect_uri}&response_type=code&scope=read&approval_prompt=force"
+        auth_url = f"{url}?client_id={self.client_id}&redirect_uri={redirect_uri}&response_type=code&scope=read&scope=activity:read_all&approval_prompt=force"
         webbrowser.open(auth_url)
-        pass
 
-    def get_strava_athlete_activities(self):
-        session = self.session()
+        handler_response = StravaAuthHandler()
+        handler_response.handle_request()
+        if handler_response.auth_code:
+            token_url = "https://www.strava.com/oauth/token"
+            payload = {
+                "client_id": handler_response.client_id,
+                "client_secret": handler_response.client_secret,
+                "code": handler_response.auth_code,
+                "grant_type": "authorization_code"
+            }
+            response = requests.post(token_url, data=payload)
+            access_token = response.json().get('access_token')
+            return access_token
+        else:
+            raise HTTPException("No auth code returned")
+        
+    def get_strava_athlete(self, access_token: str):
+        URL = "https://www.strava.com/api/v3/athlete"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        resp = self.session.get(URL, headers=headers)
+        return resp.json()
+
+    def get_strava_athlete_activities(self, access_token: str):
         URL = "https://www.strava.com/api/v3/athlete/activities"
-        headers = {"Authorization": "Bearer "}
+        headers = {"Authorization": f"Bearer {access_token}"}
         params = {
             "before": 1692061200,
             "after": 1672534800
         }
-        resp = session.get(URL, headers=headers, params=params)
+        resp = self.session.get(URL, headers=headers, params=params)
         return resp.json()
     
     def get_strava_activity_timeseries():
--- tests/strava_api.py
+++ tests/strava_api.py
@@ -1,4 +1,6 @@
 from src.app.adapters.strava_api_adapter import StravaAPIAdapter
 
 if __name__ == "__main__":
-    StravaAPIAdapter().authorize_strava_api()
+    adapter = StravaAPIAdapter()
+    access_token = adapter.authorize_strava_api()
+    activities = adapter.get_strava_athlete_activities(access_token)
