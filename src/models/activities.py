64d6cdbc8c7e4e4bb1191944eefa527b47c82e94
nichael-meilson <michaelshawnneilson@gmail.com>
2023-09-11 15:34:22 +0200

    Include activity model

--- src/models/activities.py
+++ src/models/activities.py
@@ -3,3 +3,6 @@ import pydantic
 
 class GetActivity(IDModelMixin):
     athlete_id: int = pydantic.Field(..., description="Internal Strava ID of the athlete.")
+    start_date: str = pydantic.Field(..., description="Start datetime of the activity.")
+    start_date_local: str = pydantic.Field(..., description="Local start datetime of the activity.") # Not sure which one I'll have to use
+    activity_id: int = pydantic.Field(..., description="Internal Strava ID of the activity.")
